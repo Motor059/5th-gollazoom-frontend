00efec661fd5281269e028083166dca5dce8ca6f
Motor059 <tnsgh01@gmail.com>
2026-02-22 09:05:39 +0900

    refactor: Vercel 배포 전 env 설정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
+const baseURL = import.meta.env.VITE_API_URL || 'http://192.168.158.27:8080';
+
 const api = axios.create({
-    baseURL: 'http://192.168.158.27:8080',
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json',
     },
